eb4869f55bad80ab6f20cf2292e199c103fcb256
Jeffry Gonzalez <jeff@hypertheory.com>
2020-12-15 13:56:55 -0500

    after lunch

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-list',
@@ -7,7 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private dialogRef: MatDialogRef<ListComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { filter: string }
+  ) { }
 
   ngOnInit(): void {
   }
